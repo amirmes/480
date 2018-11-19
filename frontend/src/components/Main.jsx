488ae552e23d391b1dc301522fb3bba93aadda80
amirmes <amirmesbahi32@gmail.com>
2018-11-19 10:35:38 -0800

    first

---
+++ frontend/src/components/Main.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      imageURL: '',
+    };
+
+    this.handleUploadImage = this.handleUploadImage.bind(this);
+  }
+
+/*   handleUploadImage(ev) {
+    ev.preventDefault();
+
+    const data = new FormData();
+    data.append('file', this.uploadInput.files[0]);
+    data.append('filename', this.fileName.value);
+
+    fetch('http://localhost:8000/upload', {
+      method: 'POST',
+      body: data,
+    }).then((response) => {
+      response.json().then((body) => {
+        this.setState({ imageURL: `http://localhost:8000/${body.file}` });
+      });
+    });
+  } */
+  
+  handleUploadImage(ev){
+	  ev.preventDefault();
+	  document.getElementById("myForm").reset();
+	  alert("You have uploaded your file");
+  }
+
+  render() {
+    return (
+      <form id="myForm" onSubmit={this.handleUploadImage}>
+        <div>
+          <input ref={(ref) => { this.uploadInput = ref; }} type="file" />
+        </div>
+        <div>
+          <input ref={(ref) => { this.fileName = ref; }} type="text" placeholder="Enter the desired name of file" />
+        </div>
+        <br />
+        <div>
+          <button>Upload</button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default Main;
